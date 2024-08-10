f6539f0e0a169847df853e3a7ea2916a93dc3c20
SG-Ifenna <sgifennamonanu@gmail.com>
2024-08-10 14:54:13 +0100

    made adjustments to the staking screen styling

--- src/app/pages/staking/index.tsx
+++ src/app/pages/staking/index.tsx
@@ -8,7 +8,7 @@ const Staking = () => {
     <div className=" ">
       <div className=" w-full  ">
         <div className="mx-auto w-[80%] flex gap-2 justify-around px-6  lg:w-[800px] mt-8 md:mt-14 xl:w-[1139px] h-[120px] lg:h-[170px] xl:h-[200px] bg-[#1A1A1A] bg-opacity-[60%] border border-[#FFFFFF] border-opacity-10 shadow-xl  rounded-[15px]  items-center">
-          <div className="bg-[#072018] bg-opacity-[49%] rounded-[15px] border border-[#FFFFFF] border-opacity-10   text-center w-[150px] md:w-[230px] lg:w-[270px] xl:w-[335px] h-[80px] lg:h-[120px] xl:h-[129px]">
+          <div className="bg-[#072018] bg-opacity-[49%] rounded-[15px] border border-[#FFFFFF] border-opacity-10   text-center w-[150px] md:w-[230px] lg:w-[270px] xl:w-[335px] h-[80px] lg:h-[105px] xl:h-[129px]">
             <h1 className="font-sora text-white text-opacity-[70%]  font-semibold text-[10px]/[15px] md:text-[18px]/[18px] mt-5 xl:mt-2 lg:text-[24px]/[22px] xl:text-[30px]/[37px] ">
               Token Prices
             </h1>
@@ -16,7 +16,7 @@ const Staking = () => {
               $0.0232
             </p>
           </div>
-          <div className="bg-[#072018] bg-opacity-[49%] rounded-[15px] border border-[#FFFFFF] border-opacity-10   text-center w-[150px] md:w-[230px] lg:w-[270px] xl:w-[335px] h-[80px] lg:h-[120px] xl:h-[129px]">
+          <div className="bg-[#072018] bg-opacity-[49%] rounded-[15px] border border-[#FFFFFF] border-opacity-10   text-center w-[150px] md:w-[230px] lg:w-[270px] xl:w-[335px] h-[80px] lg:h-[105px] xl:h-[129px]">
             <h1 className="font-sora text-white text-opacity-[70%]  font-semibold text-[10px]/[15px] md:text-[18px]/[18px] lg:text-[24px]/[22px] xl:text-[30px]/[37px] mt-5 xl:mt-3">
               Totals Stake
             </h1>
@@ -24,7 +24,7 @@ const Staking = () => {
               439k TP
             </p>
           </div>
-          <div className="bg-[#072018] bg-opacity-[49%] rounded-[15px] border border-[#FFFFFF] border-opacity-10   text-center w-[150px] md:w-[230px] lg:w-[270px] xl:w-[335px] h-[80px] lg:h-[120px] xl:h-[129px]">
+          <div className="bg-[#072018] bg-opacity-[49%] rounded-[15px] border border-[#FFFFFF] border-opacity-10   text-center w-[150px] md:w-[230px] lg:w-[270px] xl:w-[335px] h-[80px] lg:h-[105px] xl:h-[129px]">
             <h1 className="font-sora text-white text-opacity-[70%]  font-semibold text-[10px]/[15px] md:text-[18px]/[18px] lg:text-[24px]/[22px] xl:text-[30px]/[37px] mt-5 xl:mt-3">
               Total Stakers
             </h1>
