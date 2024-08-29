15ce855b042bf80e94a121274696ef3c67e04e33
SG-Ifenna <sgifennamonanu@gmail.com>
2024-08-29 13:53:06 +0100

    added route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
